f15fb5fcd8b750c33ec23b71b498ee09c130a627
Eugene <eugene@amberpixels.io>
2025-05-11 19:24:11 +0300

    initial basic test

--- cmd/git-undo/main_test.go
+++ cmd/git-undo/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"testing"
@@ -18,6 +19,19 @@ func run(t *testing.T, dir, cmd string, args ...string) string {
 	return string(out)
 }
 
+// runEnv is like run, but lets you inject extra env vars.
+func runEnv(t *testing.T, dir string, extraEnv []string, cmd string, args ...string) string {
+	t.Helper()
+	c := exec.Command(cmd, args...)
+	c.Dir = dir
+	c.Env = append(os.Environ(), extraEnv...)
+	out, err := c.CombinedOutput()
+	if err != nil {
+		t.Fatalf("`%s %v` failed: %v\n%s", cmd, args, err, out)
+	}
+	return string(out)
+}
+
 func TestUndoBranch(t *testing.T) {
 	// 1) create a temp dir and init a repo
 	tmp, err := os.MkdirTemp("", "gitundo-test-*")
@@ -26,16 +40,24 @@ func TestUndoBranch(t *testing.T) {
 	}
 	defer os.RemoveAll(tmp)
 
-	run(t, tmp, "git", "init", ".")
+	fmt.Println(run(t, tmp, "git", "init", "."))
+	fmt.Println(run(t, tmp, "git", "commit", "--allow-empty", "-m", "init"))
 
 	// 2) create a branch named "feature"
-	run(t, tmp, "git", "branch", "feature")
+	fmt.Println(run(t, tmp, "git", "branch", "feature"))
 
 	// sanity check: branch exists
 	pre := run(t, tmp, "git", "branch", "--list", "feature")
 	if pre == "" {
 		t.Fatal("precondition failed: feature branch was not created")
 	}
+	fmt.Println(pre)
+
+	// 3) simulate the hook log
+	runEnv(t, tmp,
+		[]string{"GIT_UNDO_INTERNAL_HOOK=1"},
+		"git-undo", "--hook=git branch feature",
+	)
 
 	// 3) run our undo
 	run(t, tmp, "git-undo")
