40196d544437c1d2feff3fa66ed3606a018c8fc8
Eugene <eugene@amberpixels.io>
2025-06-25 10:47:28 +0300

    plan described

--- scripts/integration/setup-and-test-dev.sh
+++ scripts/integration/setup-and-test-dev.sh
@@ -23,4 +23,4 @@ cd /home/testuser
 echo " Running integration tests..."
 echo "================================================================================"
 echo ""
-bats integration-test.bats #--filter "5A:" # <- uncomment to run a specific test
+bats integration-test.bats --filter "6A:" # <- uncomment to run a specific test
